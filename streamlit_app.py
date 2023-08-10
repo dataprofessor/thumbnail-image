a4f29a65b4dcc1a30fcaf4c38ddea8a2a4ab708c
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-10 18:49:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -139,7 +139,8 @@ if image_upload:
     photo_img = Image.open('photo.png').convert('RGBA')
     st.image(photo_img)
     
-    base_img.paste(photo_img, (20,20), photo_img)
+    #base_img.paste(photo_img, (20,20), photo_img)
+    base_img.paste(fixed, (20,20), fixed)
     base_img.save('final.png')
 
     final_img = Image.open('final.png')
