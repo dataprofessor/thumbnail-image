342ad648e94dc58fa938a1b08014e47b5d60fd76
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-13 08:38:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,6 +82,7 @@ with st.sidebar:
     st.subheader('Image upload')
     with st.expander('Expand'):
         image_upload = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+        image_resize = st.slider('Vertical placement', 0, 500, 100, step=10)
         image_vertical_placement = st.slider('Vertical placement', 0, 1000, 0, step=25)
         image_horizontal_placement = st.slider('Horizontal placement', -1000, 1000, 0, step=25)
 
@@ -134,7 +135,12 @@ if image_upload:
     st.subheader('Photo overlayed on Wallpaper')
     image = Image.open(image_upload)
     fixed = remove(image)
-    fixed.thumbnail([sys.maxsize, 1080], Resampling.LANCZOS)
+
+    new_height = 1080
+    new_width  = new_height * width / height
+    fixed.resize((new_width, new_height), Image.LANCZOS)
+    
+    #fixed.thumbnail([sys.maxsize, 1080], Resampling.LANCZOS)
     fixed.save(f'{img_path}/photo.png')
 
     # Overlay photo on wallpaper
